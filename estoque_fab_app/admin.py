dabe0c1f0841ed90a59bff8040329fcd4ea86f74
Douglas Silva <douglasitpro@gmail.com>
2024-12-03 14:23:03 -0300

    Adicionando os modelos no admin

--- estoque_fab_app/admin.py
+++ estoque_fab_app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Produto, Pedido, ItemPedido, Fornecedor, Compra, ItemCompra
 
-# Register your models here.
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'unidade', 'preco', 'estoque_atual', 'validade')
+    search_fields = ('nome',)
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'data_pedido', 'solicitante', 'observacoes')
+
+
+@admin.register(ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+    list_display = ('pedido', 'produto', 'quantidade', 'preco_unitario', 'subtotal')
+
+
+@admin.register(Fornecedor)
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'contato', 'email')
+
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fornecedor', 'data_compra', 'observacoes')
+
+
+@admin.register(ItemCompra)
+class ItemCompraAdmin(admin.ModelAdmin):
+    list_display = ('compra', 'produto', 'quantidade', 'preco_unitario', 'subtotal')
