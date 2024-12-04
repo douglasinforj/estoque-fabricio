74ce5ccf37a175298647a1c07e34620684b4cb26
Douglas Silva <douglasitpro@gmail.com>
2024-12-04 00:12:37 -0300

    Estilizando base.html com css, adicionando script para menu colapse(hamburguer) com responsividade

---
+++ static/js/scripts.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')     //queryselector  para css
+    sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
